646fc6acd566064e8d82dc6c10070fb02330e515
kin911014 <50545361+kin911014@users.noreply.github.com>
2020-05-12 18:18:17 +0900

    working css at contents #1

--- src/header/Popup.js
+++ src/header/Popup.js
@@ -1,22 +1,27 @@
 import React from "react";
-import cloneDeep from "lodash/cloneDeep";
 import { Remarkable } from "remarkable";
 
 export default class Popup extends React.Component {
   constructor(props) {
     super(props);
     this.md = new Remarkable();
-    this.state = { value: "", cursor: "", textSize: "" };
+    this.state = { value: "", cursor: "", textSize: 0 };
   }
-  h1event() {
-    let input_index = cloneDeep(this.state.cursor);
+  hevent(hsize) {
+    let input_index = this.getSnapshotBeforeUpdate(this.state.cursor);
     let line_index = 0;
     let textLastLine = [0];
-    let b = cloneDeep(this.state.value);
+    let pushText = "#";
+    for (let i = 1; i < hsize; i++) {
+      pushText += "#";
+    }
+    pushText += " ";
+    let b = this.getSnapshotBeforeUpdate(this.state.value);
+    console.log("error");
     b = b.split("");
-    let state2 = cloneDeep(this.state.value);
+    let state2 = this.getSnapshotBeforeUpdate(this.state.value);
     state2 = state2.split("\n");
-    let state3 = cloneDeep(this.state.value);
+    let state3 = this.getSnapshotBeforeUpdate(this.state.value);
     state3 = state3.split("");
     for (let i = 0; i < state2.length; i++) {
       line_index += state2[i].length;
@@ -25,13 +30,13 @@ export default class Popup extends React.Component {
         textLastLine.push(line_index);
       }
     }
-    console.log("textLastLine", textLastLine[textLastLine.length - 1]);
-    b.splice(textLastLine[textLastLine.length - 1], 0, "## ");
+    b.splice(textLastLine[textLastLine.length - 1], 0, pushText);
     this.setState({
       value: b.join(""),
-      textSize: state3.length + 3,
+      textSize: state3.length + hsize + 1,
     });
   }
+
   viewSet(text) {
     this.setState({
       value: text.join(""),
@@ -41,9 +46,17 @@ export default class Popup extends React.Component {
 
   cursorEvent(e) {
     let input_index = e.target.selectionStart;
-    console.log("마우스이벤트", input_index);
+    // console.log("마우스이벤트", input_index)
     this.setState({ cursor: input_index });
   }
+  keyInput(cursorPosition, key) {
+    let text = this.getSnapshotBeforeUpdate(this.state.value);
+    text = text.split("");
+
+    text.splice(cursorPosition - 1, 0, key);
+    this.viewSet(text);
+  }
+
   koreanInput(cursorPosition, key, keyAll) {
     //한글입력 중복오류 잡기
     let text = keyAll.split("");
@@ -52,7 +65,14 @@ export default class Popup extends React.Component {
   }
   deleteInput(cursorPosition, textSize, keyAll) {
     let text = keyAll.split("");
-    text.splice(cursorPosition, textSize - keyAll.split("").length);
+    text.splice(cursorPosition, textSize - (keyAll.split("").length + 1));
+    this.viewSet(text);
+  }
+  copyInput(cursorPosition, textsize, key) {
+    let text = this.getSnapshotBeforeUpdate(this.state.value);
+    text = text.split("");
+    text.splice(cursorPosition - (textsize - text.length), 0, key);
+    // console.log(text);
     this.viewSet(text);
   }
 
@@ -63,18 +83,17 @@ export default class Popup extends React.Component {
   // firstindex lastindex key
 
   editorPush(e) {
-    let state2 = e.target.value.split("");
     let textsize = e.target.value.length;
     let input_index = e.target.selectionStart;
     let key = e.target.value.substring(input_index - 1, input_index);
-    let temp = cloneDeep(this.state.textSize);
+    let temp = this.getSnapshotBeforeUpdate(this.state.textSize);
 
-    if (temp == 0) {
-      temp = textsize;
-    } else if (temp > textsize) {
+    // console.log("temp",temp,"textsize",textsize)
+    if (temp > textsize) {
       this.deleteInput(input_index, temp, e.target.value);
       // console.log("글자지우기");
     } else if (temp == textsize) {
+      // console.log("한글입력")
       this.koreanInput(input_index, key, e.target.value);
       //한글입력 오류 잡기~
     } else if (temp + 1 < textsize) {
@@ -82,23 +101,24 @@ export default class Popup extends React.Component {
         input_index - (textsize - temp),
         input_index
       );
-      input_index - (textsize - temp); //복사붙여넣기를위해 늘어난 text size를 구하여 추가된 시점부터 글자를 넣어준다.
+      this.copyInput(input_index, textsize, key);
       // console.log("복사");
-      state2.splice(input_index - (textsize - temp), 0, key);
     } else {
-      // console.log("기본입력");
-      state2.splice(input_index - 1, 1, key);
+      // console.log("기본입력",input_index);
+      this.keyInput(input_index, key);
     }
-    let state3 = state2.join(""); //배열로 잘라놓은 문자열을 하나의 문자열로 합친다.
     this.setState({
-      value: state3,
       cursor: input_index,
-      textSize: state2.length,
+      textSize: e.target.value.split("").length,
     }); //변경값을 표출한다.
   }
 
   getReMarkDown() {
     return { __html: this.md.render(this.state.value) };
+  }
+
+  getSnapshotBeforeUpdate(prevState) {
+    return prevState;
   }
 
   render() {
@@ -118,7 +138,10 @@ export default class Popup extends React.Component {
         >
           <div className="editor">
             <div className="editorButton">
-              <button onClick={this.h1event.bind(this)}>H1</button>
+              <button onClick={this.hevent.bind(this, 1)}>H1</button>
+              <button onClick={this.hevent.bind(this, 2)}>H2</button>
+              <button onClick={this.hevent.bind(this, 3)}>H3</button>
+              <button onClick={this.hevent.bind(this, 4)}>H4</button>
             </div>
             <textarea
               wrap="hard"
@@ -130,6 +153,7 @@ export default class Popup extends React.Component {
               value={this.state.value}
             ></textarea>
           </div>
+
           <div
             className="markDownView"
             dangerouslySetInnerHTML={this.getReMarkDown()}
